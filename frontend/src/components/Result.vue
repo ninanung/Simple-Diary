cd92ccf8621fe0b8e27b00c477339d08f47ac580
ninanung <ninanung@naver.com>
2018-02-24 22:18:36 +0900

    all good

--- frontend/src/components/Result.vue
+++ frontend/src/components/Result.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Confirming Your account is success! Please Login!</h1>        
+        <h1 id="wrods">Confirming Your account is success! Please Login!</h1>        
     </div>
 </template>
 <script>
@@ -11,8 +11,10 @@ export default {
 <style scoped>
     div {
         width: 100%; margin: 0 auto; height: 500px;
+        background: #376bec;
     }
     #words {
-        text-align: center;
+        text-align: center; padding: 200px 20px;
+        font-weight: bold; color: white;
     }
 </style>
