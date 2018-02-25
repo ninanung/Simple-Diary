9a3f5c1acbc017cfb0d3b88159ce2f9f7bd2a67e
ninanung <ninanung@naver.com>
2018-02-25 16:43:21 +0900

    little color change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -89,13 +89,13 @@ export default {
     }
     ul {
         list-style-type: none; margin: 0; padding: 0; 
-        overflow: hidden; background: #37EC97;
+        overflow: hidden; background: #376bec;
     }
     li {
         float: left;
     }
     li a {
-        display: block; color: #376bec; text-align: center;
+        display: block; color: white; text-align: center;
         padding: 14px 16px; text-decoration: none;
         font-weight: bold; font-size: 20px
     }
